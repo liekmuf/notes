107fbb73ec2d54011ded95bc2c6fd0792ab691db
Danylo Shulha <83084992+liekmuf@users.noreply.github.com>
2022-01-18 22:15:24 +0200

    Update initialState.js

--- src/initialState.js
+++ src/initialState.js
@@ -1,20 +1,62 @@
 export const initialState = [{
-        id: "first-item8",
-        name: "name",
-        created: "26/03/2021",
-        category: "Idea",
-        content: "content",
+        id: "i1",
+        name: "Shoppping List",
+        created: "January 18, 2022",
+        category: "Task",
+        content: "Tomatoes, Chicken fillet, cheese",
         dates: ["26/03/2021", "27/03/2021"],
         isActive: true
     },
     {
-        id: "second-item",
-        name: "Покупки",
-        created: "26/03/2021",
+        id: "i2",
+        name: "Random",
+        created: "January 18, 2022",
         category: "Random Thought",
-        content: "Контент",
-        dates: ["26/02/2021", "27/10/2000"],
+        content: "Why do young people need ripped jeans? If they fell, then why is the skin intact?",
+        dates: [],
         isActive: false
 
+    },
+    {
+        id: "i3",
+        name: "Startap",
+        created: "January 18, 2022",
+        category: "Idea",
+        content: "Online exchange of musicians",
+        dates: [],
+        isActive: true
+    },
+    {
+        id: "i4",
+        name: "Concert",
+        created: "January 18, 2022",
+        category: "Idea",
+        content: "Tommy Emmanuel concert  11/11/2021",
+        dates: ["11/11/2021"],
+        isActive: true
+    }, {
+        id: "i5",
+        name: "Test tasks",
+        created: "January 18, 2022",
+        category: "Idea",
+        content: "Complete test tasks 01/24/2022",
+        dates: ["11/11/2021"],
+        isActive: true
+    }, {
+        id: "i6",
+        name: "Dentist",
+        created: "January 18, 2022",
+        category: "Task",
+        content: "go to the dentist 12/09/2021 ",
+        dates: ["12/09/2021"],
+        isActive: false
+    }, {
+        id: "i7",
+        name: "Shoppping List",
+        created: "January 17, 2022",
+        category: "Task",
+        content: "Tomatoes, cheese",
+        dates: [],
+        isActive: true
     }
 ]
